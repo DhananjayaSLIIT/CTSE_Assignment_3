3aa1e71ced0507b04be7fa894b8e5463844d354c
IT19127538 <it19127538@my.sliit.lk>
2022-05-17 03:44:20 +0530

    commit for deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 6000
 
 app.use(cors());
 app.use(express.json());
-db_connection();
+//db_connection();
 
 const server = app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}...`);
